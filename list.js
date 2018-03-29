8aea38d7d0c669af1768f60a7a277bca28f5ab72
Julia H Smith <js44684@srcs.loc>
2018-03-29 12:19:41 -0700

    added var

---
+++ list.js
@@ -0,0 +1,7 @@
+var list = document.getElementsByTagName("ul")[0];
+var input = document.getElementById("item").textContent;
+var button = document.getElementById("add_item");
+
+function add(){
+
+}
