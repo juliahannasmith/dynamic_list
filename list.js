16ad3982abf56061b020891c8809143adb7cc65b
Julia H Smith <js44684@srcs.loc>
2018-04-02 12:25:18 -0700

    added some

--- list.js
+++ list.js
@@ -3,5 +3,7 @@ var input = document.getElementById("item").textContent;
 var button = document.getElementById("add_item");
 
 function add(){
-
+ return list.appendChild(input);
 }
+
+button.addEventListener('click', add);
